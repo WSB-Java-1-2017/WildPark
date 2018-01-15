73a8151ba89d8a3999597d40fbf79db6321eb44b
kotoniedzwiedz <35452389+kotoniedzwiedz@users.noreply.github.com>
2018-01-15 13:35:21 +0100

    add new leopard

--- src/wildpark/model/animals/mammals/Leopard.java
+++ src/wildpark/model/animals/mammals/Leopard.java
@@ -6,26 +6,62 @@ import wildpark.model.Gender;
 import wildpark.model.Meat;
 import wildpark.model.Movement;
 import wildpark.model.WildParkAreaCell;
+
+
 import wildpark.model.*;
 import wildpark.model.animals.Animal;
-import wildpark.model.animals.NocturnalAnimal;
 import wildpark.model.animals.Predator;
-import wildpark.model.animals.SsacingAnimal;
-import wildpark.model.animals.SwallowingAnimals;
+import wildpark.model.animals.NocturnalAnimal;
+import wildpark.model.animals.BirthGivingAnimal;
+import wildpark.model.animals.CarnivorousAnimal;
+import wildpark.model.animals.ChewingAnimal;
+import wildpark.model.animals.Mammal;
+import wildpark.model.animals.SwollowingAnimal;
 
 
-public class Leopard extends Mammal implements BirthGivingAnimal, CarnivorousAnimal, ChewingAnimals, SwallowingAnimals, MetaturnalAnimal, Predator, RunningAnimal, Scavenger, WalkingAnimal   {
-private final AnimalSpeciesSpecification animalSpeciesSpecification = new LampartSpecification();
 
-public Leopard( AnimalSpeciesSpecification animalSpeciesSpecification, WildParkAreaCell wildParkAreaCell, boolean isNewborn ) {
+/**
+ * The REAL animal class. This contains implementations of all abstract methods declared in all superclasses.
+ */
+public class Leopard extends Mammal implements NocturnalAnimal, Predator, BirthGivingAnimal, CarnivorousAnimal, ChewingAnimal, SwollowingAnimal{
+	private final AnimalSpeciesSpecification animalSpeciesSpecification = new LionSpecification();
+
+	//	Inherited from Meat:
+	// public Duration TIME_OF_DEATH = null;
+
+	//	Inherited from Food:
+	// public float weight;	// current animal weight
+	// private final float CALORIC_EFFICIENCY_PER_KILO = animalSpeciesSpecification.CALORIC_EFFICIENCY_PER_KILO; // okreœla liczbê kalorii z kilograma danego miêsa 
+
+	public Leopard( AnimalSpeciesSpecification animalSpeciesSpecification, WildParkAreaCell wildParkAreaCell, boolean isNewborn ) {
 		super( animalSpeciesSpecification, wildParkAreaCell, isNewborn );
 	}
 
-public AnimalSpeciesSpecification.AcceptableCellType[] getAcceptableCellTypes() {
+
+
+
+
+	public AnimalSpeciesSpecification.AcceptableCellType[] getAcceptableCellTypes() {
          return animalSpeciesSpecification.getAcceptableCellTypes();
     }
 
 	public Food getFood( WildParkAreaCell cell ) {
+		return null;
+	}
+	
+	public boolean hunt( Animal animal ) {
+		return false;
+	}
+	
+	public boolean isNocturnal() {
+		return true; //isActiveDuringTheNight;
+	}
+	
+	public int giveBirth() {	// Returns the number of newborns
+		return 0;		
+	}
+	
+	public Food chew( Food food ) {
 		return null;
 	}
 
@@ -39,17 +75,11 @@ public AnimalSpeciesSpecification.AcceptableCellType[] getAcceptableCellTypes() 
 	public void proliferate(  ) {
 	}
 
-	public boolean hunt( Animal animal ) {
-		return true;
-	}
 
 	public int digestMeat( Meat meat )	{ // return energy amount
 		return 0;
 	}
 
-	public int giveBirth() {	// Returns the number of newborns
-		return 0;		
-	}
 
 	public Food ssack( Food food ) {
 		return null;
@@ -57,13 +87,5 @@ public AnimalSpeciesSpecification.AcceptableCellType[] getAcceptableCellTypes() 
 
 	public Food swollow( Food food ) {
 		return null;
-	}
-
-	public Food chew( Food food ) {
-		return null;
-	}
-
-	public boolean isNocturnal() {
-		return true; //isActiveDuringTheNight;
 	}
 }
