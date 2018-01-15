daaa100bc18d1dbb5d2416c8b624c2cbed76d03a
stazwsb <34215612+stazwsb@users.noreply.github.com>
2018-01-15 13:39:37 +0100

    Add files via upload

---
+++ src/wildpark/Newwindow.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Newwindow extends JFrame
+{
+	public Newwindow()
+	{
+		setSize(400,400);
+		SetLocation(600,300);
+		SetVisible(true);
+		SetDefaultCloseOperation(EXIT_ON_CLOSE)
+	}
+} 
