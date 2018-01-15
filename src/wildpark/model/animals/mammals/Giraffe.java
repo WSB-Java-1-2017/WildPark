5fe2fe8b3b7e7d326c6306a53ecee9da21b7ad7a
godlajk <35452419+godlajk@users.noreply.github.com>
2018-01-15 12:51:40 +0100

    Add files via upload

---
+++ src/wildpark/model/animals/mammals/Giraffe.java
@@ -0,0 +1,53 @@
+public class Giraffe extends Mammals implements TreeEatingAnimal, WalkingAnimal, RunningAnimal, ChewingAnimal {
+
+	
+
+
+	
+
+		abstract eat(int foodWeight){
+        // tu uzupełnimy stan najedzenia.
+		}
+		
+
+     	/**
+		 *  Implementation of abstract method declared in Animal. 
+		 *  @return move speed 
+		 */
+
+
+
+		abstract void move() {
+
+		}
+
+		void eatPlant() {
+
+		}
+
+		
+
+		/**
+		 * @return Returns moving speed while walking
+		 */
+		int walk() {
+
+		}
+
+
+		/**
+		 * @return Returns moving speed while running.
+		 */
+
+		int run() {
+
+		}
+
+
+
+
+ }
+
+}
+
+}
