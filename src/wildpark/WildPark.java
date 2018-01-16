db1945bbce3fedeaf8036c3b6afa7f8676c35f35
KajZiol <35487776+KajZiol@users.noreply.github.com>
2018-01-16 20:09:54 +0100

    Dolphin

--- src/wildpark/WildPark.java
+++ src/wildpark/WildPark.java
@@ -472,6 +472,7 @@ public class WildPark extends Application {
         final int LION_COUNT = 10;
         final int CROCODILE_COUNT = 10;
         final int POLAR_BEAR_COUNT = 10;
+        final int DOLPHIN_COUNT = 10;
 
 
         for( int i=0; i<INSECT_EATING_BAT_COUNT; i++ ) {
@@ -497,6 +498,10 @@ public class WildPark extends Application {
             for( int i=0; i<POLAR_BEAR_COUNT; i++ ) {
             Animal polarbear = new PolarBear( new PolarBearSpecification(), new WildParkAreaCell( CellType.LAKE ), false );
             getAnimals().add(polarbear);
+        }
+
+        for( int i=0; i<DOLPHIN_COUNT; i++ ) {
+            Animal Dolphin = new Dolphin( new DolphinSpecification(), new WildParkAreaCell( CellType.OCEAN ), false );
         }
     }
     
