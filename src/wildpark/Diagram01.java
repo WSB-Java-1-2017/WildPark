0414ff655604eb1433f4717fbc6c547ba5bdee7a
marlepo <marcinlpotrykus@gmail.com>
2018-01-18 11:35:10 +0100

    add giraffer, update diagram and wildpark

--- src/wildpark/Diagram01.java
+++ src/wildpark/Diagram01.java
@@ -32,43 +32,54 @@ public class Diagram01{
 	XYChart.Series series4 = new XYChart.Series();
 	@SuppressWarnings("rawtypes")
 	XYChart.Series series5 = new XYChart.Series();	
-	int step = 1;
 	
-	//Horse h1 = new Horse(new Horse , null, false);
+	public static long step;
 	
-	//WildPark
 	private List <Animal> animal;
 	
 	private int horseCount = 0;
 	private int insectEatingBatCount = 0;
 	private int giraffeCount = 0;
 	
+	
 	public Diagram01( List <Animal> _animal ) {
 		this.animal = _animal;
+		this.nextStep();
 	}
 	
 	public void getCount() {
+		
 		for(Animal a : animal) {			
 			if(a.getSPECIES_NAME() == "Horse") {
 				horseCount +=1;
 				//System.out.println(++horseCount);
 			}
+			if(a.getSPECIES_NAME() == "Giraffe") {
+				giraffeCount +=1;
+				//System.out.println(++horseCount);
+			}
 			if(a.getSPECIES_NAME() == "Insect Eating Bat") { 
-				++insectEatingBatCount;
+				insectEatingBatCount +=1;
 				//System.out.println(++insectEatingBatCount);
 			}
-			System.out.println(" >>> "+insectEatingBatCount + ", " + horseCount);
+			//System.out.println(" >>> "+insectEatingBatCount + ", " + horseCount+ ", " + giraffeCount);
 		}
 	}
 	
 	
+	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void nextStep(){		
+	public void nextStep(){	
 		
+		horseCount = 0;
+		insectEatingBatCount = 0;
+		giraffeCount = 0;
+		
+		step = WildPark.getWildParkTime().toHours();
 		getCount();			
 		series1.getData().add(new XYChart.Data(step, horseCount));	
 		series2.getData().add(new XYChart.Data(step, insectEatingBatCount));
-		++step;
+		series3.getData().add(new XYChart.Data(step, giraffeCount));		
 	}
 	
     
@@ -96,12 +107,13 @@ public class Diagram01{
 		});
         
         series1.setName("Horse");
-        series2.setName("Giraffe");
-        series3.setName("Mammal3");
+        series2.setName("Insect Eating Bat");
+        series3.setName("Giraffe");
         series4.setName("Mammal4");
         series5.setName("Mammal5");
         
-        Scene scene  = new Scene( new BorderPane( lineChart, step, null, null, null ), 800,600 );
+        //Scene scene  = new Scene( new BorderPane( lineChart, step, null, null, null ), 800,600 );
+        Scene scene = new Scene( lineChart, 800, 600 );
         lineChart.setTitle("Wild Park Diagram Beta");
         lineChart.getData().addAll( series1, series2, series3, series4, series5 );    
       
