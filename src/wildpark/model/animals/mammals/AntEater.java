a287d74bd5a0f2d6001c7eb261cb5cc82fbe0e6e
Magdalena Sibila <badziakoska@users.noreply.github.com>
2018-01-18 10:47:38 +0100

    Add files via upload

---
+++ src/wildpark/model/animals/mammals/AntEater.java
@@ -0,0 +1,58 @@
+package wildpark.model.animals.mammals;
+
+import wildpark.model.*;
+import wildpark.model.animals.*;
+import wildpark.model.animals.mammals.*;
+
+public class AntEater extends Mammal implements SwallowingAnnimal{
+    private String name;
+    private int weight; //18-25kg
+    final int MAX_STARVING_HOURS_BEFORE_DEATH = 210;
+    protected int hoursSinceLastMeal = 0;
+    protected int maxEnergyPercent = 100;
+    protected int energyPercent = 100;
+    final int STANDARD_SPEED = 5;
+    final int MAX_SPEED = 60;
+    final int MAX_STAMINA = energyPercent;
+    final int AVERAGE_SCION_COUNT_IN_LITTER = 1;
+    final int MAX_AGE = 9490; //days
+    final int MIN_BREEDING_AGE = 900; //days
+    final int MAX_BREEDING_AGE = 9000; //days
+    final int MIN_SELF_GOVERNMENT_AGE = 547; // days
+    protected int age;
+    Gender gender;
+    boolean isProfilerating; // true if pregnant
+    boolean isFeedingNewborns;
+
+    public String set_name(String name){
+        return this.name = name;
+    }
+
+    public int set_weight(int weight){
+        return this.weight = weight;
+    }
+
+    public int giveBirth(){ //AntEater have always one child
+        return 1;
+    }
+
+    public void feedNewbornWithMilk(){
+        this.energyPercent -= 5;
+    }
+
+    public void ssack_Milk(){
+        this.energyPercent += 10;
+    }
+
+    public void swallow(int howManyAnt){
+        if(howManyAnt<=300){
+            this.energyPercent += 1;
+        } else if (howManyAnt <=3000){
+            this.energyPercent +=10;
+        } else {
+            this.energyPercent +=20;
+        }
+    }
+
+
+}
