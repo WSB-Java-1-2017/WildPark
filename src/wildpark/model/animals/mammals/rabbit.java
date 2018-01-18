a60a70e7640b5a2fc5ba60e452aa9aa7e4b528b9
Damian Jahn <damian.jahn@icloud.com>
2018-01-18 19:58:04 +0100

    Add files via upload
    rabbit animal

---
+++ src/wildpark/model/animals/mammals/rabbit.java
@@ -0,0 +1,23 @@
+package wildpark.model.animals.mammals;
+
+import wildpark.model.*;
+import wildpark.model.animals.*;
+
+public abstract class Rabbit extends Mammal 
+implements FloatingAnimal, NestingAnimal, FlyingAnimal
+{
+	public Rabbit( AnimalSpeciesSpecification animalSpeciesSpecification, WildParkAreaCell wildParkAreaCell, boolean isNewborn ) {
+		super( animalSpeciesSpecification, wildParkAreaCell, isNewborn );
+	}
+
+
+	public void move() { 
+	}
+
+	public void occupyNest() { 
+	}
+
+	public void leaveNest() { }
+
+	public void soar() {  }
+}
